30e8040d6e526243c113c0d62826ef0cd8221339
Shane Lillie <ignignokterr@gmail.com>
2016-09-16 22:30:25 -0700

    Iniital import

---
+++ FauxCompanion/FauxCompanion/FauxCompanion.Droid/Activities/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android;
+using Android.App;
+using Android.OS;
+
+namespace EnergonSoftware.FauxCompanion.Droid.Activities
+{
+	[Activity (Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
+	public sealed class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.activity_main);
+		}
+	}
+}
+
+
---
+++ FauxCompanion/FauxCompanion/FauxCompanion.Windows/Pages/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EnergonSoftware.FauxCompanion.Windows.Pages
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
